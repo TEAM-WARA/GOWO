e84d0fc889237e40a857df4020ebefb8c3eb4856
changuii <rhljh7410@gmail.com>
2023-11-11 18:15:57 +0900

    ✅ first commit

---
+++ back-end/gowo/src/main/java/dev/gowo/gowo/GowoApplication.java
@@ -0,0 +1,13 @@
+package dev.gowo.gowo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GowoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GowoApplication.class, args);
+	}
+
+}
---
+++ back-end/gowo/src/test/java/dev/gowo/gowo/GowoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gowo.gowo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GowoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
